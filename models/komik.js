e495d36b8a1e6e1c38558eed7e5b468f40ab2f6a
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-10-28 14:28:02 +0700

    perbaikan typo koma

--- models/komik.js
+++ models/komik.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     }, {
         tableName: 'komik',
-        timestamps: false
+        timestamps: false,
         freezeTableName: true
     });
     return Komik;
